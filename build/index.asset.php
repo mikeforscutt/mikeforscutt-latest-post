d773a4a9b6da89e8c301b21840a61aa584d4b022
mikeforscutt <mikeforscutt@gmail.com>
2022-05-24 13:56:06 +0100

    Changing no of columns

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '35403071986ed521481b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd657dd6be7f804ee0eeb');
