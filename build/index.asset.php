78d4a5e240c071ac8e3ca9f5bde9f1bec3c94cd4
mikeforscutt <mikeforscutt@gmail.com>
2022-05-24 23:11:30 +0100

    added transform feature

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6d34ddf7b4e2c207b68c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fa3037755a90cb457c14');
