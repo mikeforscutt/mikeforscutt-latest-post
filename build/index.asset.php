23da1966c1f2cc182f0c3e1468c06e85c207dd9e
mikeforscutt <mikeforscutt@gmail.com>
2022-05-24 19:12:13 +0100

    Added styling to team member and members

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd657dd6be7f804ee0eeb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a0919815788c66805b56');
